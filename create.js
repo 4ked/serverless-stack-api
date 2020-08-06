6965582b3c9a83165cd71029eaf93b3cce54b648
4ked <max.goeke0@gmail.com>
2020-08-06 14:13:46 -0500

    deploying serverless

--- create.js
+++ create.js
@@ -2,8 +2,6 @@ import * as uuid from "uuid";
 import handler from "./libs/handler-lib";
 import dynamoDb from "./libs/dynamodb-lib";
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
 export const main = handler(async (event, context) => {
   // Request body is passed in as a JSON encoded string in 'event.body'
   const data = JSON.parse(event.body);
